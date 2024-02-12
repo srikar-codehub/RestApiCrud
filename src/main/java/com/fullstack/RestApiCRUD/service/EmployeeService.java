f287d16c5ede02f4dbd278b744eb1b3cf5688b56
srikar-codehub <a.srikar.code@gmail.com>
2024-02-12 15:21:10 +0530

    CRUD implimented

--- src/main/java/com/fullstack/RestApiCRUD/service/EmployeeService.java
+++ src/main/java/com/fullstack/RestApiCRUD/service/EmployeeService.java
@@ -2,6 +2,15 @@ package com.fullstack.RestApiCRUD.service;
 
 import com.fullstack.RestApiCRUD.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
+    EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
+
+    EmployeeDto updateEmployee(Long employeeId,EmployeeDto employeeDto);
+
+    void deleteEmployee(Long employeeId);
 }
--- src/main/java/com/fullstack/RestApiCRUD/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/fullstack/RestApiCRUD/service/impl/EmployeeServiceImpl.java
@@ -1,13 +1,62 @@
 package com.fullstack.RestApiCRUD.service.impl;
 
 import com.fullstack.RestApiCRUD.dto.EmployeeDto;
+import com.fullstack.RestApiCRUD.entity.Employee;
+import com.fullstack.RestApiCRUD.exception.ResourceNotFoundException;
+import com.fullstack.RestApiCRUD.mapper.EmployeeMapper;
 import com.fullstack.RestApiCRUD.service.EmployeeService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.fullstack.RestApiCRUD.repository.EmployeeRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
-        return null;
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+
     }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("No employee exits with the given id :" + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map(employee -> EmployeeMapper.mapToEmployeeDto(employee)).collect(Collectors.toList());
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee with the given id does not exist :" + employeeId));
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+        Employee updatedEmployeeObj = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee with the given id does not exist :" + employeeId));
+        employeeRepository.deleteById(employeeId);
+
+    }
+
+
 }
